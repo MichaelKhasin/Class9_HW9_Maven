91bcd0ec7792be6e64aad620a46858dca62b35da
MichaelKhasin <khasin13@gmail.com>
2020-08-28 00:07:31 +0300

    Class9 Maven HW - Initial commit

---
+++ Maven.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+cd C:\Users\Misha\IdeaProjects\Class9_HW9_Maven
+mvn -Dtest=Class9_HW9_Maven_MyTest#exercise5 test
+pause
---
+++ src/test/java/Class9_HW9_Maven_MyTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class Class9_HW9_Maven_MyTest {
+    @Test
+    public void exercise5(){
+        System.out.println("Exercise5");
+    }
+}
